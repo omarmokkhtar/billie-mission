16538f63dd01bf1b5f0525b06472a02b919effe8
Omar Mokhtar <omar.mokhtar@afaqy.com>
2021-06-20 22:48:14 +0200

    Added the validation logic

--- app/Http/Requests/GetUTCRequest.php
+++ app/Http/Requests/GetUTCRequest.php
@@ -13,7 +13,7 @@ class GetUTCRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class GetUTCRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'entryTime' => 'required|date_format:Y-m-d H:i:s',
         ];
     }
 }
